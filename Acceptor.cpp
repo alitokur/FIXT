e85fdd715788e44e363410efeb2c9da1363327bd
badcode <alitokurr@gmail.com>
2023-04-30 16:27:18 +0300

    Move Acceptor constructor definition to Acceptor.cpp :owl:

--- Acceptor.cpp
+++ Acceptor.cpp
@@ -1,9 +1,16 @@
 #include "Acceptor.h"
 
+Acceptor::Acceptor(boost::asio::io_service& ios, unsigned short port):
+    m_ios(ios), m_acceptor(
+            m_ios, 
+            boost::asio::ip::tcp::endpoint(boost::asio::ip::address_v4::any(),port)){
+        m_acceptor.listen();
+    }
 void Acceptor::Accept()
 {
     boost::asio::ip::tcp::socket sock(m_ios);
     m_acceptor.accept(sock);
+
     Service svc;
     svc.HandleClient(sock);
 }
