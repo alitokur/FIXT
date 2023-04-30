fc2bbd9fdc37493624168442f8767b9f188dd5f2
badcode <alitokurr@gmail.com>
2023-04-30 14:56:08 +0300

    Add FIXServer launch and stop functionality to main :wolf:
    - This commit adds code to the main function to create a FIXServer instance with a specified port.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <ostream>
+#include <thread>
+#include "FIXServer.h"
 
 int main(){
-    //here we are
+    unsigned short port = 1234;
+    try{
+        FIXServer order_entry_point{ port };
+        order_entry_point.launch();
+        std::this_thread::sleep_for(std::chrono::seconds(60));
+        order_entry_point.stop();
+    }
+    catch(system::system_error &e){
+        std::cout << "Server can't launch" << std::endl;
+        std::cout << "Error Code: " << e.what() << std::endl;
+    }
     return 0;
 }
